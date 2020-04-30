e5e87062cac9be2b27732b011cf20d94a49ff3a2
xiaoni <xiaoni0601@gmail.com>
2020-04-30 13:19:10 +0800

    update lesson2

--- lesson2/SQLJoin.sql
+++ lesson2/SQLJoin.sql
@@ -107,3 +107,16 @@ JOIN orders o
 ON o.account_id = a.id
 WHERE o.standard_qty > 100 AND o.poster_qty > 50
 ORDER BY unit_price DESC;
+
+SELECT DISTINCT a.name, w.channel
+FROM accounts a
+JOIN web_events w
+ON a.id = w.account_id
+WHERE a.id = '1001'
+
+
+SELECT o.occurred_at, a.name, o.total, o.total_amt_usd
+FROM orders o
+JOIN accounts a
+ON a.id = o.account_id
+WHERE o.occurred_at BETWEEN '01-01-2015' AND '01-01-2016'
